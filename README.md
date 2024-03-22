c9ce0dc08138ddc49cf37fde9c3473dcd6d6d151
Youssef M. Atef <78027089+P4ND2X@users.noreply.github.com>
2024-03-22 23:27:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# weather_app
+ Displays weather in egypt. Forked from @noura2tarek ✨
---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+class AppConstants {
+  static const OPEN_WEATHER_API_KEY = 'a8b5d2f9121b4a0799291413241703';
+  static const BASE_URL = 'https://api.weatherapi.com/v1';
+  static const PATH = '/current.json';
+}
